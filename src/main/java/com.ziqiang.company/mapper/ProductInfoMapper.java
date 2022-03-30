5f2724fdc55b67a0139ff1a4d5f56610e5d939df
陈自强 <chenziqiang@yiqing.com>
2022-03-30 17:53:12 +0800

    新增各种xml配置，使用mybatis逆向工程生成pojo和mapper

---
+++ src/main/java/com.ziqiang.company/mapper/ProductInfoMapper.java
@@ -0,0 +1,31 @@
+package com.ziqiang.company.mapper;
+
+import com.ziqiang.company.pojo.ProductInfo;
+import com.ziqiang.company.pojo.ProductInfoExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface ProductInfoMapper {
+    int countByExample(ProductInfoExample example);
+
+    int deleteByExample(ProductInfoExample example);
+
+    int deleteByPrimaryKey(Integer pId);
+
+    int insert(ProductInfo record);
+
+    int insertSelective(ProductInfo record);
+
+    List<ProductInfo> selectByExample(ProductInfoExample example);
+
+    ProductInfo selectByPrimaryKey(Integer pId);
+
+    int updateByExampleSelective(@Param("record") ProductInfo record, @Param("example") ProductInfoExample example);
+
+    int updateByExample(@Param("record") ProductInfo record, @Param("example") ProductInfoExample example);
+
+    int updateByPrimaryKeySelective(ProductInfo record);
+
+    int updateByPrimaryKey(ProductInfo record);
+}
