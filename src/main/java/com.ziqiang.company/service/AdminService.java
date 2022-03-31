ccb01f9094f9464b9a45d497a87c9d7f866049f0
陈自强 <chenziqiang@yiqing.com>
2022-03-31 18:36:37 +0800

    md5加密，以及测试单元

---
+++ src/main/java/com.ziqiang.company/service/AdminService.java
@@ -0,0 +1,7 @@
+package com.ziqiang.company.service;
+
+import com.ziqiang.company.pojo.Admin;
+
+public interface AdminService {
+    Admin loging(String name,String pwd);
+}
---
+++ src/test/java/Md5Test.java
@@ -0,0 +1,9 @@
+import com.ziqiang.company.utils.MD5Util;
+import org.junit.Test;
+
+public class Md5Test {
+    @Test
+    public void testMd5(){
+        System.out.println(MD5Util.getMD5("000000"));;
+    }
+}
